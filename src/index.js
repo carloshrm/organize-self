6f2ead7dd4f90bff3c79519443256326a8486acb
cm <cm@CM-PC>
2021-06-04 15:49:27 -0300

    progress

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import { add, formatISO, toDate, parse } from "date-fns";
 
 const dom = {
-  taskShelf: document.getElementById("todo_container"),
-  appendTaskHTML: (currentTask) => {
+  projectShelf: document.getElementById("project_list"),
+  projectContainer: document.getElementById("project_container"),
+  projectAddForm: document.getElementById("add_project_container"),
+  projectAddNameIn: document.getElementById("project_name_form"),
+  projectLinks: document.querySelectorAll("a"),
+  projectTitle: document.getElementById("project_title"),
+  addTaskButton: document.getElementById("add_task"),
+  appendTask: (currentTask, targetProject) => {
     const singleTask = document.createElement("div");
     singleTask.innerHTML = `
         <div class="todo_task" id="${currentTask.title}">
@@ -29,18 +35,43 @@ const dom = {
           </table>
         </div>
         `;
-    dom.taskShelf.appendChild(singleTask);
+    dom.projectShelf.appendChild(singleTask);
+  },
+  appendProject: (project, id) => {
+    const myProject = document.createElement("a");
+    myProject.href = "";
+    myProject.id = id;
+    myProject.innerText = `${project.projectName}`;
+    myProject.addEventListener("click", swapProject);
+    dom.projectShelf.appendChild(myProject);
+    dom.projectAddNameIn.value = "";
+  },
+  renderProject: (project) => {
+    dom.projectTitle.innerText = project.projectName;
+    project.taskList.forEach((x) => console.log(x));
+  },
+  eventListeners: () => {
+    document
+      .getElementById("add_project_button")
+      .addEventListener("click", projectFormVisibility);
+    document
+      .getElementById("confirm_project")
+      .addEventListener("click", newProject);
+    document
+      .getElementById("cancel_project")
+      .addEventListener("click", projectFormVisibility);
+    dom.projectLinks.forEach((a) => a.addEventListener("click", swapProject));
   },
 };
 
 class Task {
-  constructor(dateDue, title, desc, priority = 0) {
-    this.dateDue = parse(dateDue);
+  constructor(dateDue, title, desc, priority) {
+    this.dateDue = parse(dateDue, "yyyy-mm-dd", new Date());
     this.title = title;
     this.desc = desc;
     this.priority = priority;
     this.dateMade = getTodayDate();
-    dom.appendTaskHTML(this);
+    // dom.appendTask(this);
   }
 }
 
@@ -49,12 +80,38 @@ function getTodayDate() {
   return now;
 }
 
-class Section {
-  constructor(task, sectionName, period) {
-    this.sectionName = sectionName;
-    this.task = task;
-    this.period = period;
+class Project {
+  static myProjects = {};
+  static idCount = 1;
+  constructor(projectName) {
+    this.projectName = projectName;
+    this.taskList = [];
   }
+  addTask = (task) => {
+    this.taskList.push(task);
+  };
+  static readProjects() {
+    this.myProjects.forEach((x) => dom.appendProject(x));
+  }
+}
 
-  addTask = () => {};
+dom.eventListeners();
+
+function newProject(e) {
+  const project = new Project(dom.projectAddNameIn.value);
+  let defaultTask = new Task(getTodayDate, "Empty Task", "description", 0);
+  project.addTask(defaultTask);
+  Project.myProjects[Project.idCount] = project;
+  dom.appendProject(project, Project.idCount);
+  projectFormVisibility();
+  Project.idCount++;
+}
+function projectFormVisibility() {
+  dom.projectAddForm.classList.toggle("add_project_container_hidden");
+}
+
+function swapProject(e) {
+  e.preventDefault();
+  console.log(e);
+  dom.renderProject(Project.myProjects[this.id]);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 module.exports = {
   entry: "./src/index.js",
+  watch: true,
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
